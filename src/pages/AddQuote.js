383872b9f9618659f1871b78004527460e6a9b90
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-22 20:04:28 +0400

    Move to list of quotes after adding a new quote

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,10 +1,13 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import {useHistory} from 'react-router-dom';
 
 const AddQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = ({author, text}) => {
     // TODO: add a new quote to store
     console.log(author, text);
-    // TODO: redirect to a list page
+    history.push('/quotes');
   };
 
   return (
