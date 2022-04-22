e8de9c2b5b1ffe6ea30026315267f24fa5569eb2
Alex Bortnik <bortnik.ar@gmail.com>
2022-04-22 14:46:42 +0400

    Header with NavLinks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Welcome from './pages/Welcome';
 import {Route} from 'react-router-dom';
 import Products from './pages/Products';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <Route path='/welcome'>
-        <Welcome/>
-      </Route>
-      <Route path='/products'>
-        <Products/>
-      </Route>
+      <Header/>
+      <main>
+        <Route path='/welcome'>
+          <Welcome/>
+        </Route>
+        <Route path='/products'>
+          <Products/>
+        </Route>
+      </main>
     </div>
   );
 }
